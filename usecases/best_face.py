b48a9713e176a09e85afce02c017a23ee9277072
uri brecher <uri.brecher@gmail.com>
2020-08-03 17:25:32 +0300

    Improve request scheduling
    the group request is only dependent on all detect requests but not on image loading
    so in this implementation the group request can be sent earlier

--- usecases/best_face.py
+++ usecases/best_face.py
@@ -1,14 +1,15 @@
 import asyncio
 from entities import FaceDetectionAPI, ImageAPI, Image, best_face, Face
-from typing import List
+from typing import List, Mapping
 
 
 async def gather_images(list_of_image_urls: List[str],
-                        image_api: ImageAPI) -> List[Image]:
+                        image_api: ImageAPI) -> Mapping[str, Image]:
     tasks = []
     for url in list_of_image_urls:
         tasks.append(image_api.load_from_url(url))
-    return await asyncio.gather(*tasks)
+    images = await asyncio.gather(*tasks)
+    return {image.url: image for image in images}
 
 
 async def gather_faces(list_of_image_urls: List[str],
@@ -21,19 +22,34 @@ async def gather_faces(list_of_image_urls: List[str],
     return flat_list
 
 
+async def group_faces(list_of_image_urls: List[str],
+                      face_detection_api: FaceDetectionAPI) -> (List[Face], List[List[str]]):
+    faces = await gather_faces(list_of_image_urls, face_detection_api)
+    # only after all faces are detect can we start sending the group request
+    face_ids = [face.id for face in faces]
+    groups = await face_detection_api.group(face_ids)
+    return faces, groups
+
+
 async def handle_best_face(list_of_image_urls: List[str],
                            face_detection_api: FaceDetectionAPI,
                            image_api: ImageAPI) -> str:
-    # TODO: handle timeouts and best effort logic
-    images, faces = await asyncio.gather(
+    """
+    :param list_of_image_urls:
+    :param face_detection_api:
+    :param image_api:
+    :return: he image url that contain the best face
+
+    using both face detection api and image loading api this use-case
+    will gather all required data and then process it using best_face function
+    """
+    image_by_url, (faces, groups) = await asyncio.gather(
         gather_images(list_of_image_urls, image_api),
-        gather_faces(list_of_image_urls, face_detection_api))
-    face_ids = [face.id for face in faces]
-    groups = await face_detection_api.group(face_ids)
+        group_faces(list_of_image_urls, face_detection_api))
 
+    # at this point we gathered all images and faces and we can proceed
+    # with creating the following mappings
     face_by_id = {face.id: face for face in faces}
-    image_by_url = {image.url: image for image in images}
     image_by_id = {face.id: image_by_url[face.url] for face in faces}
-
     best_face_id = best_face(face_by_id, image_by_id, groups)
     return face_by_id[best_face_id].url
